4c46c4790314dbb05a3a3193ec906724203e013f
rengar.liu <rengar.liu@huolala.cn>
2021-04-27 20:09:00 +0800

    fix bug

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -31,8 +31,8 @@
 				foods: [],
 				width: 10,
 				height: 10,
-				windowWidth: 0,
-				windowHeight: 0,
+				window_width: 0,
+				window_height: 0,
 				game_over: false,
 				direction: {
 					top: [-1, 0],
@@ -50,8 +50,8 @@
 			// 根据屏幕大小确定地图尺寸
 			uni.getSystemInfo({
 				success: res => {
-					this.windowWidth = res.windowWidth;
-					this.windowHeight = res.windowHeight;
+					this.window_width = res.windowWidth;
+					this.window_height = res.windowHeight;
 				}
 			});
 
@@ -80,8 +80,8 @@
 			initMap() {
 				this.maps = [];
 				this.game_over = false;
-				this.width = Math.floor(this.windowWidth / 10);
-				this.height = Math.floor(this.windowHeight * 0.7 / 10);
+				this.width = Math.floor(this.window_width / 10);
+				this.height = Math.floor(this.window_height * 0.7 / 10);
 
 				let row, value = [
 					[], 0
@@ -105,11 +105,13 @@
 				let x = Math.floor(this.height / 2);
 				let y = Math.floor(this.width / 2);
 				this.maps[x][y] = 1;
-				this.snake.unshift([x, y]);
-
-				let [i, j] = this.direction[this.next];
-				this.maps[x + i][y + j] = 1;
-				this.snake.unshift([x + i, y + j]);
+				this.maps[x - 1][y] = 1;
+				this.maps[x - 2][y] = 1;
+				this.snake = [
+					[x - 2, y],
+					[x - 1, y],
+					[x, y]
+				];
 			},
 
 			// 向前一步
@@ -117,12 +119,15 @@
 				let [i, j] = this.direction[this.next];
 				let [x, y] = this.snake[0];
 				let [row, col] = [x + i, y + j];
-				if (row == 0 ||
-					row == this.height - 1 ||
-					col == 0 ||
-					col == this.width - 1) {
+				if (row == 0 || row == this.height - 1 || col == 0 || col == this.width - 1 || this.checkSnake([row, col],
+						this.snake)) {
 					this.game_over = true;
 					clearInterval(this.timer);
+					uni.showToast({
+						title: '游戏结束！',
+						icon: 'none',
+						duration: 1000,
+					})
 					return;
 				} else {
 					this.snake.unshift([row, col]);
@@ -188,7 +193,13 @@
 			// 恢复正常速度
 			speedCommon() {
 				this.play(this.interval);
-			}
+			},
+
+			checkSnake(point, arr) {
+				return arr.some(item => {
+					return item.join() === point.join()
+				});
+			},
 		}
 	}
 </script>
